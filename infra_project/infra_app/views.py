dbb68057544fd0f1d1c08d456d584e64ab9a4599
Valeriy Stolyar <valeriistolyar@yandex.ru>
2022-08-08 15:37:41 +0300

    bug: added befin and end into ssh

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -6,4 +6,5 @@ def index(request):
 
 
 def second_page(request):
-    return HttpResponse('А это вторая страница')
+    # return HttpResponse('А это вторая страница')
+    return HttpResponse('А это вторая страница!')
